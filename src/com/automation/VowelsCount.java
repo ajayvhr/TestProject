08f03694bdc3adb62bd57e899e15b6d02d99691e
hp <hp@DESKTOP-3U85JHA.bbrouter>
2024-02-06 02:12:47 +0530

    mondy

--- src/com/automation/VowelsCount.java
+++ src/com/automation/VowelsCount.java
@@ -1,26 +1,23 @@
 package com.automation;
 
 public class VowelsCount {
-	
-	public static boolean isVowel (char t) {
-		return t=='a' || t=='e'  || t=='i'  || t=='o'  || t=='u'; 
+
+	public static boolean isVowel(char t) {
+		return t == 'a' || t == 'e' || t == 'i' || t == 'o' || t == 'u';
 	}
 
-	
-	
-	
 	public static void main(String[] args) {
-		
+
 		String str = "JavaWorld";
-		int count =0;
-		
-		for(int i =0; i<str.length() ; i++) {
-			if(isVowel(str.charAt(i))) {
+		int count = 0;
+
+		for (int i = 0; i < str.length(); i++) {
+			if (isVowel(str.charAt(i))) {
 				count++;
 			}
 		}
-			
-			System.out.println("VowelCount :" + count);
-		}
-	
+
+		System.out.println("VowelCount :" + count);
+	}
+
 }
